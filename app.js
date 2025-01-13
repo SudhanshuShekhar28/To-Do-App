bc57b43b408d9dc06ecfe399afcdeaed95eefab2
SudhanshuShekhar28 <130046450+SudhanshuShekhar28@users.noreply.github.com>
2025-01-13 11:14:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+let tasks = [];
+
+const addTask = () => {
+    const taskInput = document.getElementById('taskInput');
+    const text = taskInput.value.trim();
+
+    if (text) {
+        tasks.push({ text, completed: false });
+        taskInput.value = "";
+        updateTasksList();
+        updateStats();
+    }
+};
+
+const toggleTaskComplete = (index) => {
+    tasks[index].completed = !tasks[index].completed;
+    updateTasksList();
+    updateStats();
+};
+
+const deleteTask = (index) => {
+    tasks.splice(index, 1);
+    updateTasksList();
+    updateStats();
+};
+
+const editTask = (index) => {
+    const taskInput = document.getElementById("taskInput");
+    taskInput.value = tasks[index].text;
+
+    tasks.splice(index, 1);
+    updateTasksList();
+    updateStats();
+};
+
+const updateStats = () => {
+    const completedTasks = tasks.filter(task => task.completed).length;
+    const totalTasks = tasks.length;
+    const progress = totalTasks === 0 ? 0 : (completedTasks / totalTasks) * 100;
+    const progressBar = document.getElementById("progress");
+    progressBar.style.width = `${progress}%`;
+
+    document.getElementById("numbers").innerHTML = `${completedTasks} / ${totalTasks}`;
+    document.getElementById("percentage").innerHTML = `${Math.round(progress)}%`;
+};
+
+
+const updateTasksList = () => {
+    const taskList = document.getElementById("task-list");
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        const listItem = document.createElement("li");
+
+        listItem.innerHTML = `
+            <div class="taskItem">
+                <div class="task ${task.completed ? "completed" : ""}">
+                    <input type="checkbox" class="checkbox" ${task.completed ? "checked" : ""}/>
+                    <p>${task.text}</p>
+                </div>
+                <div class="icons">
+                    <img src="https://img.icons8.com/?size=100&id=48&format=png&color=FA5252" onClick="editTask(${index})"/>
+                    <img src="https://img.icons8.com/?size=100&id=67884&format=png&color=FA5252" onClick="deleteTask(${index})"/>
+                </div>
+            </div>
+        `;
+
+        listItem.addEventListener("change", () => toggleTaskComplete(index));
+        taskList.appendChild(listItem);
+    });
+};
+
+document.getElementById("newTask").addEventListener("click", function (e) {
+    e.preventDefault();
+    addTask();
+});
